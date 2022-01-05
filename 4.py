316e2f70e5d3890e38be4a790288923f66cf470f
Akhildattu <94892435+Akhildattu@users.noreply.github.com>
2022-01-05 09:12:04 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,16 @@
+# using for loop in list 
+l=[]
+x=int(input("Enter Element "))
+for i in range(0,x):
+    y=int(input())
+    l.append(y)
+print("List Elements ",l)
+z=int(input("Enter search Elemet "))
+for i in l:
+    pass
+print()
+if z==i:
+    print("Element is present")
+else:
+    print("Element is not present")
+
